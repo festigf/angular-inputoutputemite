15d1cbac49ed4a5fe57deea887c26884a6460ed0
festigf <gianfranco.festi@marconirovereto.it>
2020-01-14 09:06:56 +0100

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import {MatCardModule} from '@angular/material/card';
+//import './polyfills';
+
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DemoMaterialModule} from './material/material-module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { EsmaterialComponent } from './esmaterial/esmaterial.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,MatCardModule ],
+  imports:      [ 
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    DemoMaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    ],
   declarations: [ AppComponent, HelloComponent, EsmaterialComponent ],
   bootstrap:    [ AppComponent ]
 })
