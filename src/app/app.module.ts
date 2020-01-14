e1a8e9809f8de99b0da482f383e51b19d75015cf
festigf <gianfranco.festi@marconirovereto.it>
2020-01-14 08:44:27 +0100

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import {MatCardModule} from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { EsmaterialComponent } from './esmaterial/esmaterial.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule,MatCardModule ],
+  declarations: [ AppComponent, HelloComponent, EsmaterialComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/esmaterial/esmaterial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-esmaterial',
+  templateUrl: './esmaterial.component.html',
+  styleUrls: ['./esmaterial.component.css']
+})
+export class EsmaterialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
