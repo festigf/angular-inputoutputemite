cf3371f81137f3dd937caef63aa5a7e90f4a3b03
festigf <gianfranco.festi@marconirovereto.it>
2020-01-14 08:29:05 +0100

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component,OnInit Input,Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'hello',
   template: `<h1>Hello {{name}}!</h1>`,
   styles: [`h1 { font-family: Lato; }`]
 })
-export class HelloComponent  {
+export class HelloComponent implements OnInit {
   @Input() name: string;
+  @Output() nameOut : EventEmitter<string>;
+  ngOnInit(){
+    this.name+=" mod!!";
+    this.nameOut.emit(this.name);
+  }
+
 }
