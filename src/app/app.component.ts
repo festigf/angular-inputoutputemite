0d5d225924a77a71bcc575b40dd866e953dd4b41
festigf <gianfranco.festi@marconirovereto.it>
2020-01-19 16:06:29 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {interval } from 'rxjs';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular';
   nomeModificato:string="";
+  i:any=null;
+  ora:string="";
+  constructor( ){
 
+  }
+  ngOnInit(){
+    this.i=interval(1000).subscribe(
+      count=>{
+        count++;
+        this.nomeModificato+=count;
+        this.ora=Date.now().toLocaleString();
+      }
+    )
+  }
   OnNameOut(nameOut:string){
     this.nomeModificato=nameOut;
   }
